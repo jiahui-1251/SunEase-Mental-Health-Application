d39baba92531b68e6800a1a6daa5203aad5e7c71
jiahui-1251 <jiahui.tan02@gmail.com>
2024-05-30 10:20:27 +0800

    Adding initial main screen pages

--- lib/src/features/authentication/screens/widgets/navigation_menu.dart
+++ lib/src/features/authentication/screens/widgets/navigation_menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fyp/src/features/authentication/screens/profile/profile_screen.dart';
+import 'package:get/get.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class NavigationMenu extends StatelessWidget {
@@ -6,28 +8,47 @@ class NavigationMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        destinations: [
+    final controller = Get.put(NavigationController());
 
-          NavigationDestination(
-            icon: Icon(Iconsax.search_normal),
-            label: 'Resources',
-          ),
-          NavigationDestination(
-            icon: Icon(Iconsax.emoji_happy),
-            label: 'Meditation',
-          ),
-          NavigationDestination(
-            icon: Icon(Iconsax.people),
-            label: 'Forum',
-          ),          
-          NavigationDestination(
-            icon: Icon(Iconsax.profile_add),
-            label: 'Profile',
-          ),
-        ],
+    return Scaffold(
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) => controller.selectedIndex.value = index,
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Iconsax.search_normal),
+              label: 'Resources',
+            ),
+            NavigationDestination(
+              icon: Icon(Iconsax.emoji_happy),
+              label: 'Meditation',
+            ),
+            NavigationDestination(
+              icon: Icon(Iconsax.people),
+              label: 'Forum',
+            ),
+            NavigationDestination(
+              icon: Icon(Iconsax.profile_add),
+              label: 'Profile',
+            ),
+          ],
+        ),
       ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]), // Use Obx here
     );
   }
+}
+
+class NavigationController extends GetxController {
+  final RxInt selectedIndex = 0.obs; // Set default to 0 if you want the first screen to be shown initially
+
+  final screens = [
+    const HomeScreen(),
+    Container(color: Colors.purple),
+    Container(color: Colors.orange),
+    Container(color: Colors.blue),
+  ];
 }

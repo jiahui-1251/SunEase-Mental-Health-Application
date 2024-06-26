98162bbce324b84a7fbf8db16357d51a7c6f1501
jiahui-1251 <jiahui.tan02@gmail.com>
2024-06-26 16:28:02 +0800

    Daily Challenges UI (half)

--- lib/src/features/authentication/screens/daily/daily_screen.dart
+++ lib/src/features/authentication/screens/daily/daily_screen.dart
@@ -1,21 +1,146 @@
 import 'package:flutter/material.dart';
 import 'package:fyp/src/constants/colors.dart';
+import 'package:fyp/src/constants/sizes.dart';
 import 'package:fyp/src/constants/text_strings.dart';
 import 'package:fyp/src/features/authentication/screens/widgets/page_title_widget.dart';
 
-class DailyScreen extends StatelessWidget {
+class DailyScreen extends StatefulWidget {
   const DailyScreen({super.key});
 
   @override
+  _DailyScreenState createState() => _DailyScreenState();
+}
+
+class _DailyScreenState extends State<DailyScreen> {
+  bool isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return SafeArea(
       child: Scaffold(
-        appBar: PageTitleWidget(title: tDaily, backgroundColor: tOrangeColor),
+        appBar: PageTitleWidget(title: tDaily),
         body: SingleChildScrollView(
-          child: Center(
-            child: Text('This is the Daily Screen'),
-          )
-        )
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Text(
+                        "Daily Challenges",
+                        style: Theme.of(context).textTheme.titleSmall,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    const SizedBox(height: tFormHeight - 30),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            // Progress Bar
+                            child: Container(
+                              height: 20.0,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10.0),
+                                child: LinearProgressIndicator(
+                                  value: 0.7,
+                                  color: tYellowColor,
+                                  backgroundColor: tGreyColor,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Handle the button press
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(15),
+                              backgroundColor: tOrangeColor,
+                            ),
+                            child: Icon(
+                              Icons.refresh_outlined,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: tFormHeight + 10),
+
+                    // Daily Challenges Widget
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: tGreyColor,
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: ExpansionTile(
+                                title: Text(
+                                  "Say You're Great",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Write down a list of things you like about yourself and stuff you excel at.",
+                                          style: Theme.of(context).textTheme.bodyLarge,
+                                        ),
+                                        const SizedBox(height: 10),
+                                        Text(
+                                          "Challenge Benefit",
+                                          style: Theme.of(context).textTheme.titleSmall,
+                                        ),
+                                        const SizedBox(height: 5),
+                                        Text(
+                                          "Focusing on your strengths makes you feel more complete and powerful, while focusing on your shortcomings leads to feelings of incompleteness and stress.",
+                                          style: Theme.of(context).textTheme.bodyMedium,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Checkbox(
+                            value: isChecked,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                isChecked = value!;
+                              });
+                            },
+                            activeColor: tOrangeColor,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
